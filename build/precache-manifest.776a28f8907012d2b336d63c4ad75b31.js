629176f4e9cad8e25ae2773dc329e56aa2eb1bda
hejianxin <hejianxinsir@gmail.com>
2020-06-11 14:37:34 +0800

    第一次 build

---
+++ build/precache-manifest.776a28f8907012d2b336d63c4ad75b31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f5c2b71141a47c5720b13099ca708b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c67e282daaa3316bc074",
+    "url": "/static/css/main.da316280.chunk.css"
+  },
+  {
+    "revision": "e98cb61932d64b649e74",
+    "url": "/static/js/2.ec59603e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ec59603e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c67e282daaa3316bc074",
+    "url": "/static/js/main.031162dd.chunk.js"
+  },
+  {
+    "revision": "4e4481735ec3329bd25a",
+    "url": "/static/js/runtime-main.0a17a015.js"
+  }
+]);
