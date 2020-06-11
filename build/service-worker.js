2f7e3fdc2ae3adcee0c75f230a0c798c9e074cb8
hejianxin <hejianxinsir@gmail.com>
2020-06-11 14:41:21 +0800

    第二次build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.776a28f8907012d2b336d63c4ad75b31.js"
+  "/react-btn-611/build/react-btn-611/precache-manifest.586fc1b2342c8e461c04b8fab25114d8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-btn-611/build/react-btn-611/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
